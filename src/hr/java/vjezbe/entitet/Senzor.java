a853b172dbe5bae25a2ba5b3ace5f4b5ccf30cce
Petar Šlat <petar.slat@gmail.com>
2018-04-02 17:17:15 +0200

    Create abstract Senzor

---
+++ src/hr/java/vjezbe/entitet/Senzor.java
@@ -0,0 +1,41 @@
+package hr.java.vjezbe.entitet;
+
+import java.math.BigDecimal;
+
+public abstract class Senzor {
+	
+	String mjernaJedinica;
+	Byte preciznost;
+	BigDecimal vrijednost;
+	
+	public Senzor(String mjernaJedinica, Byte preciznost) {
+		super();
+		this.mjernaJedinica = mjernaJedinica;
+		this.preciznost = preciznost;
+	}
+
+	public String getMjernaJedinica() {
+		return mjernaJedinica;
+	}
+
+	public void setMjernaJedinica(String mjernaJedinica) {
+		this.mjernaJedinica = mjernaJedinica;
+	}
+
+	public Byte getPreciznost() {
+		return preciznost;
+	}
+
+	public void setPreciznost(Byte preciznost) {
+		this.preciznost = preciznost;
+	}
+
+	public BigDecimal getVrijednost() {
+		return vrijednost;
+	}
+
+	public void setVrijednost(BigDecimal vrijednost) {
+		this.vrijednost = vrijednost;
+	}
+	
+}
