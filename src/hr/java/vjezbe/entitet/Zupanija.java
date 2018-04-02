bfb231877aa95f4b8f7d6f49b5dbaa1f6dd06c75
Petar Šlat <petar.slat@gmail.com>
2018-04-02 03:16:56 +0200

    Add Zupanija

---
+++ src/hr/java/vjezbe/entitet/Zupanija.java
@@ -0,0 +1,29 @@
+package hr.java.vjezbe.entitet;
+
+public class Zupanija {
+	String naziv;
+	Drzava drzava;
+	
+	public Zupanija(String naziv, Drzava drzava) {
+		super();
+		this.naziv = naziv;
+		this.drzava = drzava;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public Drzava getDrzava() {
+		return drzava;
+	}
+
+	public void setDrzava(Drzava drzava) {
+		this.drzava = drzava;
+	}
+
+}
