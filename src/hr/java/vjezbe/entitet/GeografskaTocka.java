628c4a60ab1a4941a175ccb76c6e2f70758debec
Petar Šlat <petar.slat@gmail.com>
2018-04-02 03:08:33 +0200

    Add GeografskaTocka

---
+++ src/hr/java/vjezbe/entitet/GeografskaTocka.java
@@ -0,0 +1,30 @@
+package hr.java.vjezbe.entitet;
+
+import java.math.BigDecimal;
+
+public class GeografskaTocka {
+	BigDecimal x, y;
+
+	public GeografskaTocka(BigDecimal x, BigDecimal y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	public BigDecimal getX() {
+		return x;
+	}
+
+	public void setX(BigDecimal x) {
+		this.x = x;
+	}
+
+	public BigDecimal getY() {
+		return y;
+	}
+
+	public void setY(BigDecimal y) {
+		this.y = y;
+	}
+	
+}
