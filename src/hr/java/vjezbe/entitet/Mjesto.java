bfdc263c179bc42c3cbfcee8d022c51d0dbbf3db
Petar Šlat <petar.slat@gmail.com>
2018-04-02 03:18:37 +0200

    Add Mjesto

---
+++ src/hr/java/vjezbe/entitet/Mjesto.java
@@ -0,0 +1,29 @@
+package hr.java.vjezbe.entitet;
+
+public class Mjesto {
+	String naziv;
+	Zupanija zupanija;
+	
+	public Mjesto(String naziv, Zupanija zupanija) {
+		super();
+		this.naziv = naziv;
+		this.zupanija = zupanija;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public Zupanija getZupanija() {
+		return zupanija;
+	}
+
+	public void setZupanija(Zupanija zupanija) {
+		this.zupanija = zupanija;
+	}
+
+}
