912a8d9a36bd4a7a2bc32e0ac03ffff5074fe9e1
Petar Šlat <petar.slat@gmail.com>
2018-04-04 08:18:56 +0200

    Invoke super from extended

--- src/hr/java/vjezbe/iznimke/NiskaTemperaturaException.java
+++ src/hr/java/vjezbe/iznimke/NiskaTemperaturaException.java
@@ -5,19 +5,19 @@ public class NiskaTemperaturaException extends RuntimeException {
 	private static final long serialVersionUID = 4787462950417267751L;
 
 	public NiskaTemperaturaException() {
-
+		super();
 	}
 
 	public NiskaTemperaturaException(String message) {
-
+		super(message);
 	}
 
-	public NiskaTemperaturaException(Throwable throwable) {
-
+	public NiskaTemperaturaException(Throwable cause) {
+		super(cause);
 	}
 
-	public NiskaTemperaturaException(String message, Throwable throwable) {
-
+	public NiskaTemperaturaException(String message, Throwable cause) {
+		super(message, cause);
 	}
 
 }
--- src/hr/java/vjezbe/iznimke/VisokaTemperaturaException.java
+++ src/hr/java/vjezbe/iznimke/VisokaTemperaturaException.java
@@ -5,19 +5,19 @@ public class VisokaTemperaturaException extends Exception {
 	private static final long serialVersionUID = -2464415359284430992L;
 
 	public VisokaTemperaturaException() {
-
+		super();
 	}
 
 	public VisokaTemperaturaException(String message) {
-
+		super(message);
 	}
 
-	public VisokaTemperaturaException(Throwable throwable) {
-
+	public VisokaTemperaturaException(Throwable cause) {
+		super(cause);
 	}
 
-	public VisokaTemperaturaException(String message, Throwable throwable) {
-
+	public VisokaTemperaturaException(String message, Throwable cause) {
+		super(message, cause);
 	}
 
 }
